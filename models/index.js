858d60dd79039cbfae4aaf71b1810f7f5a48226e
Erynn Affeldt <erynn.affeldt@gmail.com>
2023-06-17 12:57:04 -0400

    Added Models index.js

--- models/index.js
+++ models/index.js
@@ -5,12 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'product_id',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+});
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongsTo(ProductTag, {
+  foreignKey: 'product_id',
+});
 // Tags belongToMany Products (through ProductTag)
+
+ProductTag.belongsTo(Product, {
+  foreignKey: 'product_id',
+});
 
 module.exports = {
   Product,
